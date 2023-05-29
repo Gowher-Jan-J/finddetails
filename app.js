647ac7e185afe00438714212d6256ac321ee1a16
PreethiRamu <preethikrishjai@gmail.com>
2023-05-29 16:31:22 +0530

    first commit

--- app.js
+++ app.js
@@ -8,51 +8,51 @@ let user = [
   {
     id: 1,
     categoryId: 7,
-    serviceName: "ac Service",
+    serviceName: "sks bus serice",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/01.jpg",
     featured: 1,
-    serviceLocation: '["Alabama","Coimbatore","Erode","Madurai","test"]',
-    servicePrice: "1200",
+    serviceLocation: '["Erode","Perundurai","Thindal"]',
+    servicePrice: "1500",
   },
   {
     id: 8,
     categoryId: 7,
-    serviceName: "men Saloon",
+    serviceName: "sks bus serice",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/09.jpg",
     featured: 1,
-    serviceLocation: '["Alabama","Coimbatore","Erode","Madurai","test"]',
+    serviceLocation:'["Erode","Trippur"]',
     servicePrice: "1200",
   },
   {
     id: 9,
     categoryId: 7,
-    serviceName: "spa for Women",
+    serviceName: "sks bus service",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/010.jpg",
     featured: 0,
-    serviceLocation: '["bangalore","Coimbatore","Erode","Madurai","trippur"]',
+    serviceLocation:'["Erode","trippur","chennai"]',
     servicePrice: "1200",
   },
   {
     id: 10,
     categoryId: 1,
-    serviceName: "bathroom Cleaning",
+    serviceName: "Tks bus service",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/02.jpg",
     featured: 0,
-    serviceLocation: '["chennai","Coimbatore","Erode"]',
+    serviceLocation:'["Chennimalai","Erode","Trippur"]',
     servicePrice: "1200",
   },
   {
     id: 11,
     categoryId: 7,
-    serviceName: "massage for Men",
+    serviceName: "Tks bus service",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/07.jpg",
@@ -63,7 +63,7 @@ let user = [
   {
     id: 12,
     categoryId: 7,
-    serviceName: "washing Machine Repair",
+    serviceName: "Tks bus service",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/012.jpg",
@@ -74,18 +74,18 @@ let user = [
   {
     id: 13,
     categoryId: 1,
-    serviceName: "kitchen Cleaning",
+    serviceName: "sks bus service",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/013.jpg",
     featured: 0,
     serviceLocation: '["bangalore","Coimbatore","Erode","chennai",]',
-    servicePrice: "1200",
+    servicePrice: "1000",
   },
   {
     id: 14,
     categoryId: 7,
-    serviceName: "fridge Repair",
+    serviceName: "sks bus service",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/014.jpg",
@@ -96,7 +96,7 @@ let user = [
   {
     id: 15,
     categoryId: 7,
-    serviceName: "full Room Painting",
+    serviceName: "sks bus service",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/017.jpg",
@@ -107,7 +107,7 @@ let user = [
   {
     id: 16,
     categoryId: 1,
-    serviceName: "full Room Cleaning",
+    serviceName: "sks bus service",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/016.jpg",
@@ -118,7 +118,7 @@ let user = [
   {
     id: 17,
     categoryId: 7,
-    serviceName: "microwave Oven Repair",
+    serviceName: "sks bus service",
     serviceDescription:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti similique libero aspernatur, amet provident inventore neque non modi dolor vitae debitis? Cumque placeat itaque amet quis atque dolorem, voluptatem in?",
     serviceImages: "/images/015.jpg",
@@ -129,7 +129,7 @@ let user = [
   {
     id: 25,
     categoryId: 6,
-    serviceName: "test",
+    serviceName: "sks bus service",
     serviceDescription: "test",
     serviceImages: "/images/RootDelivery1682683055072.jpg",
     featured: 0,
@@ -139,7 +139,7 @@ let user = [
   {
     id: 26,
     categoryId: 6,
-    serviceName: "dish antenna installation",
+    serviceName: "sks bus service",
     serviceDescription: "dish tv",
     serviceImages: "/images/RootDelivery1682702581649.jpeg",
     featured: 0,
@@ -149,7 +149,7 @@ let user = [
   {
     id: 27,
     categoryId: 3,
-    serviceName: "ssss",
+    serviceName: "sks bus service",
     serviceDescription: "ssss",
     serviceImages: "/images/RootDelivery1682703300075.png",
     featured: 0,
@@ -159,12 +159,15 @@ let user = [
 ];
 app.post("/user", async (req, res) => {
   let location = req.body.location;
-  console.log(location);
+    console.log(location);
+
 
   let a = [];
   if (Object.keys(req.body).length === 0) {
     console.log("choose location");
-    res.json({ Location: "choose location" });
+  
+
+    res.json({ Location: "choose location"});
   } else if (Object.keys(req.body).length !== 0) {
     for (let i = 0; i < user.length; i++) {
       if (user[i].serviceLocation.includes(location)) {
@@ -174,7 +177,7 @@ app.post("/user", async (req, res) => {
     if (a.length != 0) {
       res.json(a);
     } else {
-      console.log({ "message": "data not found" });
+      console.log({ "message":"data not found" });
       res.json({ "message": "data not found" });
     }
   } else {
@@ -183,10 +186,6 @@ app.post("/user", async (req, res) => {
   }
 });
 
-// for ( var i = 0; i < user.length; i++){
-//         user[i].serviceLocation=JSON.parse(user[i].serviceLocation)
-// }
-// console.log(user);
 
 app.listen(3000, () => {
   console.log("server is running");
